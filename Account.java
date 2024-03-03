2f29a259d5ecb49426c7afd9c748f0a8900a8b59
Brix Pineda <142380216+brixcyver@users.noreply.github.com>
2024-03-03 11:40:24 +0800

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,47 @@
+public class Account {
+    private String accountNumber;
+    private double balance;
+    private AccountState accountState;
+
+    public Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.accountState = new ActiveState();
+    }
+
+    public void deposit(double amount) {
+        accountState.deposit(this, amount);
+    }
+
+    public void withdraw(double amount) {
+        accountState.withdraw(this, amount);
+    }
+
+    public void suspend() {
+        accountState.suspend(this);
+    }
+
+    public void activate() {
+        accountState.activate(this);
+    }
+
+    public void close() {
+        accountState.close(this);
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setAccountState(AccountState accountState) {
+        this.accountState = accountState;
+    }
+}
