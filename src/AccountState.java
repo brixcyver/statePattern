128db37394bfc3fda13bfeb9a1210bff36a041a4
Brix Pineda <142380216+brixcyver@users.noreply.github.com>
2024-03-03 11:54:01 +0800

    Delete src directory

--- src/AccountState.java
+++
@@ -1,7 +0,0 @@
-public interface AccountState {
-    void deposit(Account account, double amount);
-    void withdraw(Account account, double amount);
-    void suspend(Account account);
-    void activate(Account account);
-    void close(Account account);
-}
